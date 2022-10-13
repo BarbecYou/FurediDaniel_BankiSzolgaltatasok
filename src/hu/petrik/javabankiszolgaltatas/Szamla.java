bdad317c8bf7b619cfee2ad3eee93eb5fe771738
Füredi Dániel <daniel.furedi03@gmail.com>
2022-10-13 08:39:39 +0200

    Szamla osztaly kesz van

---
+++ src/hu/petrik/javabankiszolgaltatas/Szamla.java
@@ -0,0 +1,21 @@
+package hu.petrik.javabankiszolgaltatas;
+
+public abstract class Szamla extends BankiSzolgaltatas {
+
+    protected int aktualisEgyenleg;
+
+    public Szamla(Tulajdonos tulajdonos) {
+        super(tulajdonos);
+    }
+
+    public int getAktualisEgyenleg() {
+        return aktualisEgyenleg;
+    }
+    public void befizet(int osszeg){
+        aktualisEgyenleg += osszeg;
+    }
+    public boolean kivesz(int osszeg){
+        return true;
+    }
+
+}
